140b3ad1e663558084ea71007e8c692fe6897e4c
AGUSTIN FULCO <47069276@ort>
2023-04-20 09:33:58 -0300

    boton

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,10 @@
-alert("Hola");
-
+const textoProducto = document.getElementById("producto");
+textoProducto.onkeyup = () => {
+    if (textoProducto.value != "") {
+        boton = document.getElementById("boton");
+        boton.classList.remove("disabled")
+    }
+    else {
+        boton.classList.add("disabled")
+    }
+}
