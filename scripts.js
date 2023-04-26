f8ea378f22efb417f3e16451e3bcdeb44c236c83
AGUSTIN FULCO <47069276@ort>
2023-04-26 09:08:39 -0300

    casi terminado

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
-const textoProducto = document.getElementById("producto");
-textoProducto.onkeyup = () => {
-    if (textoProducto.value != "") {
+const inputProducto = document.getElementById("producto");
+inputProducto.onkeyup = () => {
+    if (inputProducto.value != "") {
         boton = document.getElementById("boton");
         boton.classList.remove("disabled")
     }
@@ -10,9 +10,27 @@ textoProducto.onkeyup = () => {
 }
 
 document.getElementById("boton").onclick = () => {
-    axios.get(`https://dummyjson.com/products/search?q=${textoProducto.value}`)
+    axios.get(`https://dummyjson.com/products/search?q=${inputProducto.value}`)
     .then(function (response) {
-        console.log(response.data);
+        response.data.products.forEach(element => {
+            const listaProductos = document.getElementById("listaProductos")
+            const productos = document.createElement("div");
+            productos.innerHTML = 
+            `<div id="producto" class="card" style="width: 18rem;">
+                <img id="imagenProducto" src="${element.thumbnail}" class="card-img-top">
+                <div class="card-body">
+                <h5 id="tituloProducto"class="card-title">${element.title}</h5>
+                <button id="botonVerMas"class="btn btn-success">Ver mas</button>
+                </div>
+            </div>`
+            //const tituloProducto = document.getElementById("tituloProducto")
+            //const imagenProducto = document.getElementById("imagenProducto")
+            //tituloProducto.innerHTML = 
+            //imagenProducto.src = ;
+            listaProductos.appendChild(productos);
+        });
+        
     })
 }
 
+botonVerMas = document.getElementById("botonVermas")
